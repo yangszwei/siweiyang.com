7c3502a040b4c316b687dac288ea62e88cc13717
楊斯惟 <yangszwei@outlook.com>
2022-05-20 14:09:18 +0800

    Create home page

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{css,js}",
+  ],
+  theme: {
+    fontFamily: {
+      'display': ["'Noto Sans TC'", "sans-serif"],
+    },
+  },
+};
