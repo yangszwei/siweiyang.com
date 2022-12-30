7a1bd6b6d63c961a6b6b11f219d039538eaf00a1
楊斯惟 <yangszwei@outlook.com>
2022-12-30 10:40:26 +0800

    Update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,35 @@
 <script>
+	import GithubIcon from '@iconify-icons/mdi/github';
 	import Icon from '@iconify/svelte';
+	import TelegramIcon from '@iconify-icons/mdi/telegram';
+	import TwitterIcon from '@iconify-icons/mdi/twitter';
 
-	/** @type {import('./$types').PageData} */
-	export let data;
+	const links = [
+		{ color: '#1da1f2', icon: TwitterIcon, href: 'https://twitter.com/yangszwei' },
+		{ color: '#333', icon: GithubIcon, href: 'https://github.com/yangszwei' },
+		{ color: '#08C', icon: TelegramIcon, href: 'https://t.me/yangszwei' },
+	];
 </script>
 
 <svelte:head>
 	<title>楊斯惟 Si-Wei Yang</title>
 </svelte:head>
 
-<div class="mx-auto box-content max-w-4xl px-4 py-12 md:px-12">
-	<header>
-		<h1 class="select text-5xl leading-relaxed">Hi, 我是楊斯惟</h1>
-		<p class="text-lg leading-relaxed tracking-wide text-neutral-600">國北護 資訊管理系</p>
-	</header>
-	<main class="my-8">
-		<p class="italic text-neutral-600">(待完成)</p>
-	</main>
-	<footer>
-		<ul class="flex flex-row flex-wrap gap-4">
-			{#each data.links ?? [] as link}
-				<li>
-					<a
-						href={link.href}
-						target="_blank"
-						rel="noopener noreferrer"
-						class="block rounded-full p-1 text-2xl transition-colors duration-100
-							hover:bg-neutral-300 active:bg-neutral-300"
-					>
-						<Icon icon={link.icon} style="color: {link.color};" />
-					</a>
-				</li>
-			{/each}
-		</ul>
-	</footer>
-</div>
+<header class="mx-auto box-content max-w-4xl px-4 py-12 md:px-12">
+	<h1 class="text-5xl leading-relaxed">Hi, 我是楊斯惟。</h1>
+	<p class="text-lg leading-relaxed tracking-wide text-neutral-600">國北護 資訊管理系</p>
+	<ul class="mt-8 flex flex-row flex-wrap gap-4">
+		{#each links as { color, icon, href }}
+			<li>
+				<a
+					{href}
+					target="_blank"
+					rel="noopener noreferrer"
+					class="block rounded-full p-1 text-2xl outline-0 transition-colors duration-100 hover:bg-neutral-300 active:bg-neutral-300"
+				>
+					<Icon {icon} style="color: {color};" />
+				</a>
+			</li>
+		{/each}
+	</ul>
+</header>
